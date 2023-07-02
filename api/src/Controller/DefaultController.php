331464c1902e07aa741ae3ee92030b46da59ff25
malashenkoEvgeniy <111k1808@gmail.com>
2023-07-02 22:46:39 +0300

    add crud order

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DefaultController extends AbstractController
 {
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     #[Route('/', name: 'app_default')]
     public function index(): Response
     {
+        $orders = $this->entityManager->getRepository(Order::class)->findAll();
+//        dump($orders[0]->getProduct()->getTitle());die;
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'orders' => $orders,
         ]);
     }
 }
--- api/src/Entity/Order.php
+++ api/src/Entity/Order.php
@@ -49,4 +49,5 @@ class Order
 
         return $this;
     }
+
 }
